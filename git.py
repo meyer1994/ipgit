f4476a4d3cb775d8766dfce99210444d3100199a
Joao Meyer <1994meyer@gmail.com>
2020-09-02 00:35:29 -0300

    Added docs

--- git.py
+++ git.py
@@ -8,6 +8,17 @@ from subprocess import PIPE
 
 
 async def bare(path: str, hook: bool = False) -> None:
+    """
+    Creates a bare git repository on the passed path
+
+    If the hook argument is True, the function will add the IPFS add command
+    to the update hook fo the repo
+
+    Args:
+        path: String with path where to create the repository
+        hook: Boolean wether or not to create the update hook. Defaults to
+        False
+    """
     path = shlex.quote(path)
     args = f'git init --bare {path}'
     args = shlex.split(args)
@@ -18,7 +29,7 @@ async def bare(path: str, hook: bool = False) -> None:
 
     hook = '''
     #!/bin/sh
-    echo "Root IPFS hash:"
+    echo "IPFS hash:"
     ipfs add --recursive --quieter --pin $PWD
     '''
 
@@ -31,24 +42,31 @@ async def bare(path: str, hook: bool = False) -> None:
 
 
 async def info(service: str, path: str) -> IO:
+    """ Returns repository information for smart HTTP backend as a stream """
     path = shlex.quote(path)
     args = f'{service} --stateless-rpc --advertise-refs {path}'
     args = shlex.split(args)
 
     result = subprocess.run(args, stdout=PIPE, check=True)
 
+    # Adapted from:
+    #   https://github.com/schacon/grack/blob/master/lib/grack.rb
     data = b'# service=' + service.encode()
     datalen = len(data) + 4
     datalen = b'%04x' % datalen
     data = datalen + data + b'0000' + result.stdout
+
     return io.BytesIO(data)
 
 
 async def service(service: str, path: str, stream: IO) -> IO:
+    """ Used by `git-upload-pack` and `git-receive-pack` when updating refs """
     path = shlex.quote(path)
     args = f'{service} --stateless-rpc {path}'
     args = shlex.split(args)
 
+    # We pass the stream bit by bit to avoid loading
+    # everything into memory at once
     proc = subprocess.Popen(args, stdin=PIPE, stdout=PIPE)
     async for data in stream:
         proc.stdin.write(data)
